5d3bf9add4f10ca2cd3dcff46fab7f54ae7f9975
Anthony Martini <anthonycm2022@gmail.com>
2024-10-29 17:41:12 -0400

    removed ejs

--- server/index.js
+++ server/index.js
@@ -28,9 +28,7 @@ app.use(cors({
   }
 }));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
