b5d6ffb58eb10492b0bc3e9887111c5664336312
Anthony Martini <anthonycm2022@gmail.com>
2024-10-29 18:52:17 -0400

    d

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,21 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(`Express on Vercel: ${process.env.NEXT_PUBLIC_DB_NAME}`);
   console.log(process.env.NEXT_PUBLIC_DB_NAME);
+
+  const config = {
+    user: process.env.NEXT_PUBLIC_DB_USER,
+    password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+    server: process.env.NEXT_PUBLIC_DB_SERVER,
+    port: parseInt(process.env.NEXT_PUBLIC_DB_PORT, 10),
+    database: process.env.NEXT_PUBLIC_DB_NAME,
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+};
+console.log(config);
 });
 
 
