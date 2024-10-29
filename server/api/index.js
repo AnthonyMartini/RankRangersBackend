c5aaa85dd35b96b3e2ecc7a2d3d86c2b83f40656
Anthony Martini <anthonycm2022@gmail.com>
2024-10-28 21:55:59 -0400

    test

--- server/api/index.js
+++ server/api/index.js
@@ -1,63 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-const cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const app = express();
+
 var indexRouter = require('./routes/index');
 var coursesRouter = require('./routes/courses');
 var instructorRouter = require('./routes/instructors');
 var coursesearchRouter = require('./routes/coursesearch');
 
-var app = express();
-
-// List of allowed origins (local and production domains)
-const allowedOrigins = [
-  'http://localhost:3001',      // Local development
-  'https://rank-rangers.vercel.app/' // Production domain
-];
-app.use(cors({
-  origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps, Postman, etc.)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-          return callback(null, true);
-      } else {
-          return callback(new Error('Not allowed by CORS'));
-      }
-  }
-}));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use('/', indexRouter);
 app.use('/courses', coursesRouter);
 app.use('/instructors', instructorRouter);
 app.use('/coursesearch', coursesearchRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
+
