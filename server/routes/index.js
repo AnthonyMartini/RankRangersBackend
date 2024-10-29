8221f44b837a27e884aead0e66781a604ebbb2f2
Anthony Martini <anthonycm2022@gmail.com>
2024-10-29 18:00:29 -0400

    update routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_NAME,
+  authentication: {
+      type: 'default'
+  },
+  options: {
+      encrypt: true
+  }
+};
+console.log("Database configuration:", config);
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
+
 
 module.exports = router;
