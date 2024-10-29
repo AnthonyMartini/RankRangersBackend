7986b6a28812f5a3535bfdb3af05517158018ed1
Anthony Martini <anthonycm2022@gmail.com>
2024-10-29 18:42:49 -0400

    t

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,11 +4,11 @@ const sql = require('mssql');
 require('dotenv').config();
 // SQL Server configuration
 const config = {
-    user: process.env.NEXT_PUBLIC_DB_USER,
-    password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-    server: process.env.NEXT_PUBLIC_DB_SERVER,
-    port: parseInt(process.env.NEXT_PUBLIC_DB_PORT, 10),
-    database: process.env.NEXT_PUBLIC_DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
     authentication: {
         type: 'default'
     },
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,24 +4,11 @@ const express = require("express");
 
 const app = express();
 
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  port: parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_NAME,
-  authentication: {
-      type: 'default'
-  },
-  options: {
-      encrypt: true
-  }
-};
-console.log(config);
-console.log(process.env.DB_NAME);
+
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send(`Express on Vercel: ${process.env.NEXT_PUBLIC_DB_NAME}`);
+  console.log(process.env.NEXT_PUBLIC_DB_NAME);
 });
 
 
